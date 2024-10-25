4b01a0117f63dd1ccb10b2cb181e3d2036b66f42
荒井美香 <mika8500@yk.commufa.jp>
2024-10-25 12:20:06 +0900

    first

---
+++ src/components/organism/user/UserCard.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Card } from '../../atoms/card/Card';
+import styled from 'styled-components';
+import { UserIconWithName } from './UserIconWithName';
+
+export const UserCard = (props) => {
+    const { user } = props;
+    return (
+        <Card>
+            <UserIconWithName image={user.image} name={user.name}/>
+            <SDl>
+                <dt>メール</dt>
+                <dd>{user.email}</dd>
+                <dt>TEL</dt>
+                <dd>{user.phone}</dd>
+                <dt>会社名</dt>
+                <dd>{user.company.name}</dd>
+                <dt>サイト</dt>
+                <dd>{user.website}</dd>
+            </SDl>
+        </Card>
+    )
+};
+
+const SDl = styled.dl`
+    text-align: left;
+    margin-bottom: 0;
+    dt {
+        float: left;
+    }
+    dd {
+        padding-left: 32px;
+        padding-bottom: 8px;
+        overflow-wrap: break-word;
+    }
+`;
---
+++ src/components/pages/Users.jsx
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+import { SearchInput } from "../molecules/SearchInput";
+import { UserCard } from "../organism/user/UserCard"
+;
+const users = [...Array(10).keys()].map(((val) => {
+    return {
+        id: val,
+        name: `mika-${val}`,
+        image: "./images/leo.jpg",
+        email: "mail@address.com",
+        phone: "000-1234-5678",
+        company: {
+            name: "株式会社ーーー"
+        },
+        website: "https://google.com"
+    }
+}));
+
+export const Users = () => {
+  return (
+    <SContainer>
+        <h2>ユーザー一覧</h2>
+        <SearchInput />
+        <SUserArea>
+            {users.map((user) => (
+                <UserCard key={users.id} user={user} />
+            ))}
+        </SUserArea>
+    </SContainer>
+  )
+};
+
+const SContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 24px;
+`;
+
+const SUserArea = styled.div`
+    padding-top: 40px;
+    width: 100%;
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+    grid-gap: 20px;
+`;
